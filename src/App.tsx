6debb46251114e31e40eb8138c0e6d29e3daa3d8
Iuri Mendes <fcoiuri@gmail.com>
2022-10-31 19:58:32 -0300

    feat: create page of create account

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Home } from './components/Home';
+import { CreateAccount } from './components/CreateAccount';
 
 function App() {
   return (
     <React.Fragment>
-      <Home/>
+      {/* <Home/> */}
+      <CreateAccount/>
     </React.Fragment>
   );
 }
